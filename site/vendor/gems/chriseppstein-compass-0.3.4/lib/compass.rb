a2df50efb8ffba9782ba7980537d0df541c9a394
Chris Eppstein <chris@eppsteins.net>
2008-12-14 21:34:09 -0800

    Freeze gems.

---
+++ site/vendor/gems/chriseppstein-compass-0.3.4/lib/compass.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sass'
+require File.join(File.dirname(__FILE__), 'sass_extensions')
+
+['core_ext', 'version'].each do |file|
+  require File.join(File.dirname(__FILE__), 'compass', file)
+end
+
+module Compass
+  extend Compass::Version
+  def base_directory
+    File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  end
+  module_function :base_directory
+end
+
+require File.join(File.dirname(__FILE__), 'compass', 'frameworks')
+# make sure we're running inside Merb
+require File.join(File.dirname(__FILE__), 'compass', 'merb') if defined?(Merb::Plugins)  
+
