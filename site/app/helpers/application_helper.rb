95ab9dad908b764e7409847e57bd78d6a62423df
Chris Eppstein <chris@eppsteins.net>
2008-12-23 15:54:42 -0800

    Google Analytics Tracking Code.

--- site/app/helpers/application_helper.rb
+++ site/app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def analytics_tag
+    render :partial => "shared/analytics"
+  end
 end
