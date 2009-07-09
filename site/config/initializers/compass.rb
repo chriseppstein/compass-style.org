2c9d8cbc33c854969d81cccc13a80642e864585e
Chris Eppstein <chris@eppsteins.net>
2009-07-09 09:17:21 -0700

    Start using some of 0.8's new features.

--- site/config/initializers/compass.rb
+++ site/config/initializers/compass.rb
@@ -1,8 +1,12 @@
 require 'compass'
-# If you have any compass plugins, require them here.
+
 Compass.configuration do |config|
   config.project_path = RAILS_ROOT
   config.sass_dir = 'app/stylesheets'
   config.css_dir = 'public/stylesheets'
+  config.images_dir = 'public/images'
+  config.http_path = '/'
+  config.http_images_path = "/images"
 end
+
 Compass.configure_sass_plugin!
